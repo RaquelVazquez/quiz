9e1525e4f197c77a8e14d66aa9f45e8c52e4b404
ben <ruben.gomez@canselleiro.org>
2015-07-13 22:49:17 +0200

    añadimos pagina de autores

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET  page autor */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
+});
+
+router.get('/author', function(req, res) {
+  res.render('author', { title: 'Cŕeditos' });
 });
 
 module.exports = router;
